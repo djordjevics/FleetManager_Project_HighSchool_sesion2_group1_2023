3903e7fa1f0e1f99259261bff13470f9091f45c9
Duck1712 <matej.25medakovic@gmail.com>
2023-11-17 13:32:20 +0100

    Vehicle Model

---
+++ FleetManager/Models/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿namespace FleetManager.Models
+{
+    public class Vehicle
+    {
+        public Guid Id { get; set; }
+
+        public string FuelType { get; set; } = default!;
+
+        public int FuelCapacity { get; set; }
+
+        public string FuelUnit { get; set; } = default!;
+
+        public string Manufacturer { get; set; } = default!;
+
+        public string Model { get; set; } = default!;
+
+        public string License {  get; set; } = default!;
+
+    }
+}
