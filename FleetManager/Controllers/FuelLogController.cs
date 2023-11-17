42b020636fb680a374fea2228ecbcda28df9efbe
Tara Bogdanic <bogdanic.tara@eprosveta.rs>
2023-11-17 13:03:48 +0100

    Empty FuelLogController

---
+++ FleetManager/Controllers/FuelLogController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FleetManager.Controllers
+{
+    public class FuelLogController : ControllerBase
+    {
+        private readonly ILogger<FuelLogController> _logger;
+        public FuelLogController(ILogger<FuelLogController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
