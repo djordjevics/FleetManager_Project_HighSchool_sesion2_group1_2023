017b940ef65b45fed7c323d4d3df9d69912a0ae5
Tara Bogdanic <bogdanic.tara@eprosveta.rs>
2023-11-20 15:20:58 +0100

    Merged main

--- FleetManager/Controllers/VehicleController.cs
+++ FleetManager/Controllers/VehicleController.cs
@@ -1,23 +1,29 @@
 using FleetManager.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FleetManager.Controllers
-{
-    [ApiController]
-    [Route("[controller]/{id}")]
-    public class VehicleController : ControllerBase
-    {
-        private readonly ILogger<VehicleController> _logger;
+namespace FleetManager.Controllers;
 
-        public VehicleController(ILogger<VehicleController> logger)
-        {
-            _logger = logger;
-        }
-        [HttpPut(Name = "UpdateVehicle")]
-        public ActionResult Update(Guid id, PutVehicle updateRequest)
-        {
-            updateRequest.Id = id;
-            throw new NotImplementedException();
-        }
+[ApiController]
+[Route("[controller]/{id}")]
+public class VehicleController : ControllerBase
+{
+    private readonly ILogger<VehicleController> _logger;
+
+    public VehicleController(ILogger<VehicleController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPut(Name = "UpdateVehicle")]
+    public ActionResult Update(Guid id, PutVehicle updateRequest)
+    {
+        updateRequest.Id = id;
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteVehicle(int id)
+    {
+        throw new NotImplementedException();
     }
 }
