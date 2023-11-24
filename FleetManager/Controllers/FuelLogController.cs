1157b27c451de90e29513546bd65dbc7e1d11416
k4vl4d1 <radujkov.vladimir@jjzmaj.edu.rs>
2023-11-24 15:03:41 +0100

    Implemented FuelLogService to FuelLogController

--- FleetManager/Controllers/FuelLogController.cs
+++ FleetManager/Controllers/FuelLogController.cs
@@ -1,4 +1,5 @@
-﻿using FleetManager.Models;
+﻿using BusinessLogic.Services;
+using FleetManager.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FleetManager.Controllers
@@ -8,15 +9,19 @@ namespace FleetManager.Controllers
     public class FuelLogController : ControllerBase
     {
         private readonly ILogger<FuelLogController> _logger;
+
+        private IFuelLogService _fuelLogService;
+        
         public FuelLogController(ILogger<FuelLogController> logger)
         {
             _logger = logger;
+            _fuelLogService = new FuelLogService();
         }
 
         [HttpGet]
         public ActionResult GetAllFuelLogs()
         {
-            throw new NotImplementedException();
+           return Ok(_fuelLogService.GetAllFuelLogs());
         }
 
         [HttpGet(Name = "GetById")]
@@ -43,5 +48,6 @@ namespace FleetManager.Controllers
         {
             throw new NotImplementedException();
         }
+        
     }
 }
