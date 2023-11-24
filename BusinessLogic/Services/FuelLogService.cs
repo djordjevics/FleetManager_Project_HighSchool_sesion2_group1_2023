1e9db27c8f0fee2637bc65d03280d462d18b4e80
k4vl4d1 <radujkov.vladimir@jjzmaj.edu.rs>
2023-11-24 14:28:30 +0100

    Vehicle and FuelLog service - interfaces implemented

---
+++ BusinessLogic/Services/FuelLogService.cs
@@ -0,0 +1,31 @@
+﻿using FleetManager.Models;
+
+namespace BusinessLogic.Services;
+
+public class FuelLogService : IFuelLogService
+{
+    public FuelLog Add(FuelLog fuelLog)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<FuelLog> GetAllFuelLogs()
+    {
+        throw new NotImplementedException();
+    }
+
+    public FuelLog GetById(Guid Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<FuelLog> GetLogsByCarId()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BusinessLogic/Services/VehicleService.cs
@@ -0,0 +1,26 @@
+﻿using FleetManager.Models;
+
+namespace BusinessLogic.Services;
+
+public class VehicleService : IVehicleService
+{
+    public Vehicle AddVehicle(Guid Id, AddVehicle AddRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteVehicle(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Vehicle> Get()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Vehicle Update(Guid id, PutVehicle updateRequest)
+    {
+        throw new NotImplementedException();
+    }
+}
