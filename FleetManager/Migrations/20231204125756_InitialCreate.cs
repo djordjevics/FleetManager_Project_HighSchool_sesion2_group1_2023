babb146215d88e6f4015ef3ea88756197308ba3c
Duck1712 <matej.25medakovic@gmail.com>
2023-12-04 14:09:57 +0100

    Connected to database

---
+++ FleetManager/Migrations/20231204125756_InitialCreate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "vehicleData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FuelType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FuelCapacity = table.Column<int>(type: "int", nullable: false),
+                    FuelUnit = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Manufacturer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    License = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_vehicleData", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "fuelLogData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FuelAdded = table.Column<int>(type: "int", nullable: false),
+                    FuelUnit = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VehicleId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    VehicleDataId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_fuelLogData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_fuelLogData_vehicleData_VehicleDataId",
+                        column: x => x.VehicleDataId,
+                        principalTable: "vehicleData",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_fuelLogData_VehicleDataId",
+                table: "fuelLogData",
+                column: "VehicleDataId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "fuelLogData");
+
+            migrationBuilder.DropTable(
+                name: "vehicleData");
+        }
+    }
+}
